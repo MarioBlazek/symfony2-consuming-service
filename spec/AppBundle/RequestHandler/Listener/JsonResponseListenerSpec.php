9412c2c8f0b64adb5991ef7da287a93059a90641
Marek <blazek.mario@gmail.com>
2015-04-14 10:50:33 +0200

    Created JsonResponseListener

---
+++ spec/AppBundle/RequestHandler/Listener/JsonResponseListenerSpec.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace spec\AppBundle\RequestHandler\Listener;
+
+use AppBundle\RequestHandler\Event\ReceivedResponse;
+use AppBundle\RequestHandler\Response;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class JsonResponseListenerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('AppBundle\RequestHandler\Listener\JsonResponseListener');
+    }
+
+    function it_handles_json_response(ReceivedResponse $receivedResponse, Response $response)
+    {
+        $receivedResponse->getResponse()->willReturn($response);
+        $response->getHeader('Content-Type')->willReturn('application/json');
+        $response->getBody()->willReturn('{"data":[ ]}');
+        $response->setBody(array('data' => array()))->shouldBeCalled();
+    }
+
+    function it_does_not_handle_non_json_response(ReceivedResponse $receivedResponse, Response $response)
+    {
+        $receivedResponse->getResponse()->willReturn($response);
+        $response->getHeader('Content-Type')->willReturn('text/html');
+        $response->getBody()->shouldNotBeCalled();
+
+        $this->onReceivedResponse($receivedResponse);
+    }
+
+    function it_fails_to_handle_invalid_json(ReceivedResponse $receivedResponse, Response $response)
+    {
+        $receivedResponse->getResponse()->willReturn($response);
+        $response->getHeader('Content-Type')->willReturn('application/json');
+        $response->getBody()->willReturn('{"data":[');
+
+        $exception = 'Exception';
+
+        $this->shouldThrow($exception)->duringOnReceivedResponse($receivedResponse);
+    }
+}
---
+++ src/AppBundle/RequestHandler/Listener/JsonResponseListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\RequestHandler\Listener;
+
+use AppBundle\RequestHandler\Event\ReceivedResponse;
+
+class JsonResponseListener
+{
+
+    public function onReceivedResponse(ReceivedResponse $receivedResponse)
+    {
+        $response = $receivedResponse->getResponse();
+        $contentType = $response->getHeader('Content-Type');
+        if ( false === strpos($response->getHeader('Content-Type'), 'application/json') ) {
+            return;
+        }
+
+        $body = $response->getBody();
+        $json = json_decode($body, true);
+
+        if ( json_last_error() ) {
+            throw new \Exception("Invalid JSON: $body");
+        }
+
+        $response->setBody($json);
+    }
+}
